7695a3e994991b729ae8764a6e44a1a22e064f7b
md muhaiminul <muhai4959@gmail.com>
2024-05-21 21:14:14 +0600

    modify frontend

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: { "2xl": "1248px" },
+		},
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				primary: "var(--primary)",
+			},
+			borderRadius: {
+				DEFAULT: "var(--radius)",
+				lg: "calc(var(--radius) + 4px)",
+				xl: "calc(var(--radius) + 8px)",
+			},
+		},
 	},
 	plugins: [],
 };
