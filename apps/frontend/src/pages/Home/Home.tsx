587535557f0dbd572769aab5fedae1cf25a976c2
aargon007 <muhai4959@gmail.com>
2024-06-24 09:24:25 +0600

    modify styles, config and add button ui

--- apps/frontend/src/pages/Home/Home.tsx
+++ apps/frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+import Button from "@/components/UI/Button";
+
 const Home = () => {
-    return <div>hello this is home</div>;
+    return (
+        <div>
+            {/* header  */}
+            <nav className="flex items-center justify-between h-[80px] px-5">
+                <ul className="list-none flex items-center justify-between gap-x-5">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Components</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Stack</Link>
+                    </li>
+                </ul>
+                <h2>MERN Turbo Boilerplate</h2>
+                {/* dark mode */}
+                <button>dark</button>
+            </nav>
+
+            <div className="grid grid-cols-3">
+                <div>
+                    <Button>
+                        Button
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
