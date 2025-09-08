e55d093afaa9c9c56c6a5617f2d2e835f50fa30f
aargon007 <muhai4959@gmail.com>
2025-09-08 11:50:34 +0600

    frontend dockerfile issue fixed

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
+    plugins: [react(), tailwindcss(), tsconfigPaths()],
+    server: {
+        host: true,
+        watch: {
+            usePolling: true
+        },
+        strictPort: true,
+        port: 3000
     }
 });
