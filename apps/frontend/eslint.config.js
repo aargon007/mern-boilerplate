b1ec65b2131bb959d79b91e1ad60b9be4a9df7ed
aargon007 <muhai4959@gmail.com>
2025-11-08 20:07:13 +0600

    pkg update

---
+++ apps/frontend/eslint.config.js
@@ -0,0 +1,55 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser
+        }
+    }
+]);
+
+// module.exports = {
+//     root: true,
+//     env: { browser: true, es2020: true },
+//     extends: [
+//         'eslint:recommended',
+//         'plugin:@typescript-eslint/recommended',
+//         'plugin:react-hooks/recommended'
+//     ],
+
+//     parser: '@typescript-eslint/parser',
+
+//     plugins: {
+//         'react-hooks': reactHooks,
+//         'react-refresh': reactRefresh
+//     },
+//     // plugins: ['react-refresh'],
+//     rules: {
+//         '@typescript-eslint/no-explicit-any': 'off',
+//         'no-unused-vars': 'off',
+//         '@typescript-eslint/no-unused-vars': [
+//             'warn',
+//             { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+//         ],
+//         // '@typescript-eslint/no-unused-vars': 'off',
+//         'react-refresh/only-export-components': [
+//             'warn',
+//             { allowConstantExport: true }
+//         ]
+//     }
+// };
+
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,7 +5,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), tsconfigPaths()],
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+        tailwindcss(),
+        tsconfigPaths()
+    ],
     server: {
         host: true,
         watch: {
